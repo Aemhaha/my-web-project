292b271b8e7c66e39d000753ba6760fa45f89556
Natcha Saosuk <natchasaosuk@Natchas-MacBook-Pro.local>
2022-02-08 15:44:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//document.write("Natcha"); // การเขียน
+//document.write("<h1>Java Script เบื้องต้น</h1>"); //
+//lert("Click Aollow");
+
+//console.log("Hello javaScript"); 
+//console.error("เจอไวรัส");
+//console.warn("ผิดพลาด");
+// test
+/* test */
+
+
+let money;
+ let name; // สร้างตัวแปรแต่ไม่กำหนดค่า
+ name = "Siam"; //กำหนดข้อความลงไปเก็บใน name
+let age = 50;
+let _for = "สอนเขียนเว็บ"
+
+ console.log(name);
+ document.write(name);
+ document.write("อายุ : ", age);
+
+ document.write("Befor = ",_for);
+ _for = "สอนเขียนเว็บอิอิ";
+ document.write("After = ",_for);
